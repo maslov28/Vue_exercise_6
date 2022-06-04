f153a2093d2f958bb85b985f20fb39036df7c412
Artem <artem.fatefulchoice@mail.ru>
2022-06-04 14:37:15 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <p>сверстать два инпута и привязать к ним соответствующие реактивные переменные</p>
+  <p>вывести ниже блок с суммой, которая расcчитывается с помощью computed</p>
+
+  <input type="number" v-model="a">
+  <input type="number" v-model="b">
+  <div>{{calculate}}</div>
+
+  <new-component></new-component>
+
+</template>
+
+<script>
+import NewComponent from './components/NewComponent'
+export default {
+  data() {
+    return {
+      a: '',
+      b: ''
+    }
+  },
+  computed: {
+    calculate() {
+      if (this.a == '') {
+        return this.b;
+      }
+      else if (this.b == '') {
+        return this.a;
+      }
+      return this.a + this.b;
+    }
+  },
+  components: {NewComponent}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <input type="number" v-model="a">
+  <input type="number" v-model="b">
+  <div v-if="calculate !== 0">{{calculate}}</div>
+</template>
+
+<script>
+
+import {ref, computed} from 'vue'
+
+export default {
+    setup() {
+        const a = ref(null)
+        const b = ref(null)
+
+        const calculate = computed(() => {
+            return a.value + b.value
+        }) 
+
+        return {a, b, calculate}
+    }
+}
+</script>
+
+<style>
+
+</style>
